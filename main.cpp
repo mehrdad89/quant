58ebd800ee1416f508a1897a36117a623baa6626
Mehrdad Meh <mehrdad89@users.noreply.github.com>
2023-04-20 14:32:51 +0200

    Create main.cpp
    main function that prompts the user for input and calculates the option price:

---
+++ main.cpp
@@ -0,0 +1,16 @@
+int main() {
+    double S, K, r, sigma, T;
+    std::cout << "Enter the underlying asset price: ";
+    std::cin >> S;
+    std::cout << "Enter the option strike price: ";
+    std::cin >> K;
+    std::cout << "Enter the risk-free interest rate: ";
+    std::cin >> r;
+    std::cout << "Enter the volatility of the underlying asset: ";
+    std::cin >> sigma;
+    std::cout << "Enter the time to expiration in years: ";
+    std::cin >> T;
+    double callPrice = blackScholesCall(S, K, r, sigma, T);
+    std::cout << "The price of the European call option is: " << callPrice << endl;
+    return 0;
+}
